f90b83294638c9f2e1ac287e180f0434941c7bed
Kiffin Gish <kiffin.gish@planet.nl>
2017-09-11 18:33:14 +0200

    Import associations.

--- 18-jira_update_association.rb
+++ 18-jira_update_association.rb
@@ -35,25 +35,57 @@ puts "Total Assembla associations: #{@associations_assembla.length}"
 
 # Collect ticket statuses
 @relationship_names = {}
-@relationship_tickets = []
+@relationship_tickets = {}
 @associations_assembla.each do |association|
   ticket_id = association['ticket_id'].to_i
+  ticket1_id = association['ticket1_id'].to_i
+  ticket2_id = association['ticket2_id'].to_i
+  if ticket1_id != ticket_id && ticket2_id != ticket_id
+    goodbye("ticket1_id (#{ticket1_id}) != ticket_id (#{ticket_id}) && ticket2_id (#{ticket2_id}) != ticket_id (#{ticket_id})")
+  end
   name = association['relationship_name']
   if @relationship_names[name].nil?
     @relationship_names[name] = 0
   else
     @relationship_names[name] += 1
   end
-  @relationship_tickets[ticket_id] = [] unless @relationship_tickets.include?(ticket_id)
-  @relationship_tickets[ticket_id] << association
+  @relationship_tickets[ticket_id] = { associations: {} } if @relationship_tickets[ticket_id].nil?
+  @relationship_tickets[ticket_id][:associations][name] = [] if @relationship_tickets[ticket_id][:associations][name].nil?
+  @relationship_tickets[ticket_id][:associations][name] << {
+    ticket: ticket1_id == ticket_id ? 2 : 1,
+    ticket_id: ticket1_id == ticket_id ? ticket2_id : ticket1_id
+  }
 end
 
-puts "Total relationships: #{@relationship_names.keys.length}"
-@relationship_names.each do |name|
-  puts "* #{name}: #{@relationship_names[name]}"
+puts "Total relationship names: #{@relationship_names.keys.length}"
+@relationship_names.each do |item|
+  puts "* #{item[0]}: #{item[1]}"
 end
 
-puts "Total tickets: #{@relationship_tickets.length}"
+puts "\nTotal tickets: #{@relationship_tickets.keys.length}"
+@relationship_tickets.keys.sort.each do |ticket_id|
+  names = []
+  @relationship_tickets[ticket_id][:associations].keys.each do |name|
+    items = []
+    list = @relationship_tickets[ticket_id][:associations][name]
+    list.each do |item|
+      items << "#{item[:ticket]}:#{item[:ticket_id]}"
+    end
+    names << "#{name}:[#{items.join(',')}]"
+  end
+  puts "#{ticket_id}: #{names.join(',')}"
+end
+
+# 0 - Parent (ticket2 is parent of ticket1 and ticket1 is child of ticket2)
+# 1 - Child  (ticket2 is child of ticket1 and ticket2 is parent of ticket1)
+# 2 - Related (ticket2 is related to ticket1)
+# 3 - Duplicate (ticket2 is duplication of ticket1)
+# 4 - Sibling (ticket2 is sibling of ticket1)
+# 5 - Story (ticket2 is story and ticket1 is subtask of the story)
+# 6 - Subtask (ticket2 is subtask of a story and ticket1 is the story)
+# 7 - Dependent (ticket2 depends on ticket1)
+# 8 - Block (ticket2 blocks ticket1)
+# 9 - Unknown
 
 @jira_associations_tickets = []
 
